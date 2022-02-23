3483959f7b5df08a6bb00952cd0514f91c7ecb25
Mitchanok <6003599@mborijnland.nl>
2022-02-23 11:04:52 +0100

    homepage
    ..

---
+++ js/home.js
@@ -0,0 +1,7 @@
+const menuBar = document.querySelector('.bar');
+      const head = document.querySelector('.head');
+
+      menuToggle.addEventListener('click', () => {
+        menuToggle.classList.bar('active');
+        showcase.classList.bar('active');
+      })
