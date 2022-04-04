f64b9c98ed3a890c00b781efef6df87d065a1788
Mitchanok <6003599@mborijnland.nl>
2022-04-04 14:50:28 +0200

    ..

--- js/omreken.js
+++ js/omreken.js
@@ -1,3 +1,29 @@
 document.querySelector("#from_numerals")
 document.querySelector("#conv_numerals_ext > p:nth-child(1)")
 document.querySelector("#conv_numerals_ext")
+
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1393151-1']);
+_gaq.push(['_setCustomVar', 1, 'Page language', 'nl', 3]);
+_gaq.push(['_setCustomVar', 2, 'Page reference', 'numerals', 3]);
+_gaq.push(['_setCustomVar', 3, 'Site', 'Desktop', 3]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+
+awR(resultWidth);
+function jsReady() {
+  dRl = true;
+  for (var i=0; i<dR.length; i++) dR[i]();
+}
+if (!dRl && typeof(convert) === "function") {
+  jsReady();
+}
